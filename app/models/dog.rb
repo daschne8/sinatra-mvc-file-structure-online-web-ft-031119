ef6731c84c803e9915bd3becaa7aa661aaf09c4d
Daniel Schneider <daschne8@gmail.com>
2019-04-22 15:06:01 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,16 @@
 class Dog
   attr_accessor :name, :breed, :age
 
+  @@all = []
+
   def initialize(name,breed,age)
     @name, @breed, @age = name,breed,age
   end
   # def initialize(attributes)
   #   attributes.each{|key,value| self.send("#{key}=",value)}
   # end
+  def self.all
+    @@all
+  end
+  
 end
